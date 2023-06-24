6ae63e839b6c0352c0e7c7f2ae0dd60486c36edb
itzniel <w0817788@myscc.ca>
2023-06-23 23:50:14 -0400

    Updated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Test010817788UwaguosaAluyi-Osa';
+export class AppComponent implements OnInit {
+  title = 'Grocery Store';
+  ngOnInit(): void {
+    console.log("Before initializing firestore");
+
+    // TODO: Replace the following with your app's Firebase project configuration
+    // See: https://support.google.com/firebase/answer/7015592
+    const firebaseConfig = {
+        FIREBASE_CONFIGURATION
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+
+    // Initialize Cloud Firestore and get a reference to the service
+    const db = getFirestore(app);
+
+    console.log("After initializing firestore");
+  }
 }
