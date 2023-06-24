f32dbed041bf117d030e76e8d3e74044e3f2654d
itzniel <w0817788@myscc.ca>
2023-06-23 23:50:23 -0400

    Updated file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,12 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from './filter.pipe';
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs, Firestore } from 'firebase/firestore/lite';
+import { getAuth } from "firebase/auth";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,9 +19,39 @@ import { FilterPipe } from './filter.pipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD9iVgNvBV4cV1U9AD1rme2A2VNQXudYOQ",
+  authDomain: "grocery-f8f99.firebaseapp.com",
+  projectId: "grocery-f8f99",
+  storageBucket: "grocery-f8f99.appspot.com",
+  messagingSenderId: "819159396240",
+  appId: "1:819159396240:web:927781a829e2c723b20d15",
+  measurementId: "G-257PX61SQB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Get a list of cities from your database
+async function getCities(db: Firestore) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
